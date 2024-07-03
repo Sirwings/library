9ecd007821bffd24d4a90f6cc801d62bf55a61a9
Sirwings <148321796+Sirwings@users.noreply.github.com>
2024-07-03 16:26:59 +0200

    Create signup-success.view.php

---
+++ signup-success.view.php
@@ -0,0 +1,19 @@
+<?php 
+include "header.php"; 
+include "index.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<h1 class="success">Signup Réussi !</h1>
+
+<a href="login.view.php"><p class="redirect">Redirection sur la page de login</p></a>
+</body>
+</html>
