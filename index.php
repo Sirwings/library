52f7d8fa022a65e38a484ae5ce9c5c473820c17d
Sirwings <148321796+Sirwings@users.noreply.github.com>
2024-07-03 16:23:10 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+$isLoggedIn = isset($_SESSION['user']) && $_SESSION['user']['logged'];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <?php if ($isLoggedIn): ?>
+        <script src="app.js" defer></script>
+        <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+    <?php endif; ?>
+</head>
